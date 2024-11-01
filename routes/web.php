e97fc2b6e795ab15e061b180de3dc50836219d64
Tada Chaipanya <fendersdtua@hotmail.com>
2024-11-01 11:12:11 +0700

    Comment route '/about'

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ExampleController;
+// use App\Http\Controllers\ExampleController;
 
 // Route::get('/', function () {
 //     return view('homepage');
 // });
 Route::get('/', [UserController::class, 'showCorrectHomepage']);
-Route::get('/about', [ExampleController::class, 'aboutPage']);
+// Route::get('/about', [ExampleController::class, 'aboutPage']);
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
